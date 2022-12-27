48138ce921166f9edadfdfbdfab74fd9b71bb8c9
Hithem EL KAMEL <hel-kame@e1r11p6.clusters.42paris.fr>
2022-12-27 20:41:16 +0100

    fix(events_leaks)

--- includes/so_long.h
+++ includes/so_long.h
@@ -6,7 +6,7 @@
 /*   By: hel-kame <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/23 18:46:25 by hel-kame          #+#    #+#             */
-/*   Updated: 2022/12/27 20:10:06 by hel-kame         ###   ########.fr       */
+/*   Updated: 2022/12/27 20:40:49 by hel-kame         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,14 @@
 # include <stdio.h>
 # include <mlx.h>
 
+# define W 119
+# define S 115
+# define A 97
+# define D 100
+# define ESC 65307
+
 typedef struct s_mlx {
-	int		pas;
+	int		move;
 	void	*mlx;
 	void	*win;
 }			t_mlx;
--- src/main.c
+++ src/main.c
@@ -6,12 +6,11 @@
 /*   By: hel-kame <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/26 19:16:10 by hel-kame          #+#    #+#             */
-/*   Updated: 2022/12/27 20:14:47 by hel-kame         ###   ########.fr       */
+/*   Updated: 2022/12/27 20:37:51 by hel-kame         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
-#include <X11/X.h>
 
 int	handle_no_event(t_mlx *mlx)
 {
@@ -22,20 +21,22 @@ int	handle_no_event(t_mlx *mlx)
 int	close_window(t_mlx *mlx)
 {
 	mlx_destroy_window(mlx->mlx, mlx->win);
+	mlx_destroy_display(mlx->mlx);
+	free(mlx->mlx);
 	exit(0);
 }
 
 int	handle_keypress(int keycode, t_mlx *mlx)
 {
 	static int	i;
-
-	if (keycode == 0xff1b)
+	
+	if (keycode == ESC)
 		mlx_destroy_window(mlx->mlx, mlx->win);
-	if (keycode == 119 || keycode == 115 || keycode == 97 || keycode == 100)
+	if (keycode == W || keycode == S || keycode == A || keycode == D)
 	{
 		i++;
-		mlx->pas = i;
-		ft_printf("Nombre de pas = %d\n", mlx->pas);
+		mlx->move = i;
+		ft_printf("Nombre de move = %d\n", mlx->move);
 	}
 	return (0);
 }
@@ -52,12 +53,9 @@ int	main(void)
 	}
 	mlx.win = mlx_new_window(mlx.mlx, 640, 480, "Hello World");
 	if (!mlx.win)
-	{
-		free(mlx.win);
 		exit(-1);
-	}
 	mlx_loop_hook(mlx.mlx, &handle_no_event, &mlx);
-	mlx_hook(mlx.win, 17, 0, &close_window, &mlx);
+	mlx_hook(mlx.win, 17, 1L << 17, &close_window, &mlx);
 	mlx_hook(mlx.win, 2, 1L << 0, &handle_keypress, &mlx);
 	mlx_loop(mlx.mlx);
 	mlx_destroy_display(mlx.mlx);
